47b03df5dc68eeaf47f444bf4a6164980ded1c23
tadekgrzybowski <tadeusz.grzybowski@bsr.edu.pl>
2021-09-28 14:11:14 +0200

    chamowanie dodane

--- rakiety.py
+++ rakiety.py
@@ -10,26 +10,28 @@ import matplotlib.pyplot as plt
 
 
 class Rocket():
-    def __init__(self, engine_thrust, rocket_mass, destination, delta_czas):
+    def __init__(self, engine_thrust, rocket_mass, destination, delta_czas, v_k):
         self.e_t = engine_thrust
+        self.e_t_2 = engine_thrust
         self.r_m = rocket_mass
         self.x_k = destination
         self.d_t = delta_czas
         self.y = 0
         self.v = 0
+        self.v_k = v_k
         self.g_a = -9.8 # przyspieszenie ziemskie
         self.r_a = 0 # przyspieszenie rakiety
         self.i = 0
         self.data = [self.d_t, [], [], self.r_a, []]
-
+        
     def launch(self):
         while True:
-            if self.y > 798:
-            	print(self.y)
             self.main_calc()
             self.check_event()
             self.append_data()
-            if self.y > self.x_k + 5 or self.y < 0:
+            if self.y > self.x_k + 5 or self.y < -5:
+                break
+            if self.i > 100000:
                 break
 
     def main_calc(self):
@@ -38,12 +40,15 @@ class Rocket():
         self.y += self.v * self.d_t
 
     def check_event(self):
- 
         b_d = -(self.v**2) / (2 * self.g_a)
-        if round(b_d, 10) + round(self.y, 10) > self.x_k -0.1 and round(b_d, 10) + round(self.y, 10) < self.x_k:
+        l_d = -(self.v_k**2 - self.v**2) / (2 * ((self.e_t_2 / self.r_m) + self.g_a))
+        if ((round(b_d) + round(self.y)) == self.x_k and self.e_t != 0):
             self.e_t = 0
-      
-
+            print("elo")
+        if round(self.y) - round(l_d) == 0 and self.v < 0:
+            self.e_t = self.e_t_2
+            print(self.y, l_d, self.v)
+   
     def append_data(self):
         self.i += 1
         self.data[1].append(self.y)
@@ -57,6 +62,6 @@ class Rocket():
         plt.show()
 
 if __name__ == '__main__':
-    rocket = Rocket(35000000, 3000000, 800, 0.001)
+    rocket = Rocket(35000000, 3000000, 800, 0.001, 1)
     rocket.launch()
     rocket.plot()
